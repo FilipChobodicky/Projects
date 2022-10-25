87e070e59d161f5ed5e3cbc33998ef851a35c307
Filip Chobodicky <chobodicky@delegit.cz>
2022-10-25 21:42:28 +0200

    Add def to code for auction

--- silent_auction.py
+++ silent_auction.py
@@ -13,17 +13,32 @@ while lets_continue == "yes":
     lets_continue = input("I there any other auditor? Type 'yes' or 'no'")
     if lets_continue == "no":
         os.system("clear")
-print(dictionary)
+# print(dictionary)
 
-highest_offer = 0
-winner = ""
-for key in dictionary:
-    if dictionary[key] >  highest_offer:
-       highest_offer =  dictionary[key]
-       winner = key
-print(f"Winner of the silent auction is {winner} with offer {highest_offer} USD")
-# print(highest_offer)
-# print(winner)
+#pouziti def ************
+
+def highest_offer(offers_dictionary):
+    highest_offer = 0
+    winner = ""
+    for key in offers_dictionary:
+        if offers_dictionary[key] > highest_offer:
+            highest_offer = offers_dictionary[key]
+            winner = key
+    print(f"Winner of the silent auction is {winner} with offer {highest_offer} USD")
+
+highest_offer(dictionary)
+
+
+
+# bez def ************
+
+# highest_offer = 0
+# winner = ""
+# for key in dictionary:
+#     if dictionary[key] >  highest_offer:
+#        highest_offer =  dictionary[key]
+#        winner = key
+# print(f"Winner of the silent auction is {winner} with offer {highest_offer} USD")
      
 
 
